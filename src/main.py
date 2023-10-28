e1c76345f8d63b05500cda6d0a73fc8165393d2f
Wesley Damasceno <124368605+wesleydmscn@users.noreply.github.com>
2023-10-28 11:25:40 -0300

    Creating base Gnome App for Hash Decrypt
    * initial files
    
    * delete .flatpak-builder cache folder
    
    * add .gitignore

---
+++ src/main.py
@@ -0,0 +1,84 @@
+# main.py
+#
+# Copyright 2023 Wesley Damasceno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+import gi
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Gio, Adw
+from .window import HashDecryptWindow
+
+
+class HashDecryptApplication(Adw.Application):
+    """The main application singleton class."""
+
+    def __init__(self):
+        super().__init__(application_id='org.gnome.hashdecrypt',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
+        self.create_action('about', self.on_about_action)
+        self.create_action('preferences', self.on_preferences_action)
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        win = self.props.active_window
+        if not win:
+            win = HashDecryptWindow(application=self)
+        win.present()
+
+    def on_about_action(self, widget, _):
+        """Callback for the app.about action."""
+        about = Adw.AboutWindow(transient_for=self.props.active_window,
+                                application_name='Hash Decrypt',
+                                application_icon='org.gnome.hashdecrypt',
+                                developer_name='Open Source Hash Checker',
+                                version='0.1.0',
+                                developers=['Dahlton S', 'Wesley Damasceno'],
+                                copyright='© 2023 Dahlton S')
+        about.present()
+
+    def on_preferences_action(self, widget, _):
+        """Callback for the app.preferences action."""
+        print('app.preferences action activated')
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+
+def main(version):
+    """The application's entry point."""
+    app = HashDecryptApplication()
+    return app.run(sys.argv)
